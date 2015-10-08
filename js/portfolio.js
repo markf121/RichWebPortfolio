354c5804f6e3988b5f971dafed1b08b74fb9ae3c
Mark <C12474988@mydit.ie>
2015-10-08 21:03:49 +0100

    Extend show/hide functionality to About page.

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,3 +2,9 @@ function showIndex () {
 	document.getElementById('about').style.display = 'none';
 	document.getElementById('contact').style.display = 'none';
 }
+
+function showAbout () {
+	document.getElementById('about').style.display = 'block';
+	document.getElementById('index').style.display = 'none';
+	document.getElementById('contact').style.display = 'none';
+}
