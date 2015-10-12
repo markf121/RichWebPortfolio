60761949d3e5fcf711dc294caf5b791f518e4aad
Mark <C12474988@mydit.ie>
2015-10-12 10:43:18 +0100

    Added JS functionality to show and hide education and contact page

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,10 +1,28 @@
 function showIndex () {
 	document.getElementById('about').style.display = 'none';
 	document.getElementById('contact').style.display = 'none';
+	document.getElementById('education').style.display = 'none';
 }
 
 function showAbout () {
 	document.getElementById('about').style.display = 'block';
 	document.getElementById('index').style.display = 'none';
 	document.getElementById('contact').style.display = 'none';
+	document.getElementById('education').style.display = 'none';
+}
+
+function showContact () {
+	document.getElementById('contact').style.display = 'block';
+	document.getElementById('about').style.display = 'none';
+	document.getElementById('education').style.display = 'none';
+	document.getElementById('index').style.display = 'none';
+	
+}
+
+function showEdu () {
+	document.getElementById('contact').style.display = 'none';
+	document.getElementById('about').style.display = 'none';
+	document.getElementById('education').style.display = 'block';
+	document.getElementById('index').style.display = 'none';
+	
 }
